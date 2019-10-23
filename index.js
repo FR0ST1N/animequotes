61ea762758fde025d0a99c51d28e54f4eb615f86
FR0ST1N <iamfrostin@gmail.com>
2019-10-23 16:40:36 +0530

    style fix for index.js

--- index.js
+++ index.js
@@ -1,45 +1,44 @@
-"use strict";
+'use strict'
 
-const quotes = require('./quotes');
+const quotes = require('./quotes')
 
-function randomIntFromInterval(min, max)
-{
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function randomIntFromInterval (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function searchData(input_key, input_val){
-    let result = [];
-    for(let i = 0; i < quotes.length; i++){
-        if(quotes[i][input_key] == input_val){
-            result.push(quotes[i]);
-        }
+function searchData (inputKey, inputVal) {
+  const result = []
+  for (let i = 0; i < quotes.length; i++) {
+    if (quotes[i][inputKey] === inputVal) {
+      result.push(quotes[i])
     }
-    if(result.length > 0){
-        return result;
-    }else{
-        return {"Error": "No matches were found"};
-    }
+  }
+  if (result.length > 0) {
+    return result
+  } else {
+    return { Error: 'No matches were found' }
+  }
 }
 
-function randomQuote(){
-    return quotes[randomIntFromInterval(1, quotes.length)];
+function randomQuote () {
+  return quotes[randomIntFromInterval(1, quotes.length)]
 }
 
-function getQuote(id){
-    return searchData("id", id);
+function getQuote (id) {
+  return searchData('id', id)
 }
 
-function getQuotesByAnime(anime){
-    return searchData("anime", anime);
+function getQuotesByAnime (anime) {
+  return searchData('anime', anime)
 }
 
-function getQuotesByCharacter(character){
-    return searchData("name", character);
+function getQuotesByCharacter (character) {
+  return searchData('name', character)
 }
 
 module.exports = {
-    randomQuote: randomQuote,
-    getQuote: getQuote,
-    getQuotesByAnime: getQuotesByAnime,
-    getQuotesByCharacter: getQuotesByCharacter
+  randomQuote: randomQuote,
+  getQuote: getQuote,
+  getQuotesByAnime: getQuotesByAnime,
+  getQuotesByCharacter: getQuotesByCharacter
 }
